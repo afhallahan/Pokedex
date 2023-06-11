630a36fce6f0eea1802bb5a2accb8dcbdaa2eb91
afhallahan <allyson.hallahan@gmail.com>
2023-06-11 14:37:07 -0600

    add formatting breaks and paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,41 @@
 let pokemonList = [
-    {
-        name: 'Kingdra', 
-        height: 1.8, 
-        types: ['dragon', 'water'],
-    },
-    {
-        name: 'Wigglytuff',
-        height: 12,
-        types: ['fairy', 'normal'],
-    },
-    {
-        name: 'Rhydon',
-        height: 120,
-        types: ['rock', 'ground'],
-    },
+  {
+    name: "Kingdra",
+    height: 1.8,
+    types: ["dragon", "water"],
+  },
+  {
+    name: "Wigglytuff",
+    height: 12,
+    types: ["fairy", "normal"],
+  },
+  {
+    name: "Rhydon",
+    height: 120,
+    types: ["rock", "ground"],
+  },
 ];
-
+//list pokemon name and height
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(+
-        pokemonList[i].name + 
-        +
-        "height:" +
-        pokemonList[i].height +
-       
-    );
+  document.write(
+    "<p>" +
+      pokemonList[i].name +
+      "<br>" +
+      "height:" +
+      pokemonList[i].height +
+      "</p>"
+  );
 
-    {
-        if (pokemonList[i].height >= 15) {
-          document.write( 
-            pokemonList[i].name +  
-              pokemonList[i].height +  
-              "-Wow, that's big!" 
-             
-          );
-        }
-      }
+
+//conditional if pokemon is bigger than 15
+{
+    if (pokemonList[i].height >= 15) {
+      document.write( "<p>" +
+        pokemonList[i].name + "<br>" +
+          pokemonList[i].height + "<br>" +
+          "-Wow, that's big!" +
+          "</p>"
+      );
     }
+  }
+}
