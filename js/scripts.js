2b061d774d760d34acc7a6080fd0015346d5e18c
afhallahan <allyson.hallahan@gmail.com>
2023-06-12 13:19:30 -0600

    add spaces to string

--- js/scripts.js
+++ js/scripts.js
@@ -22,20 +22,18 @@ for (let i = 0; i < pokemonList.length; i++) {
     "<p>" +
       pokemonList[i].name +
       "<br>" +
-      "height:" +
+      "height:" + " " + " " +
       pokemonList[i].height +
       "</p>"
   );
 
   //conditional if pokemon is bigger than 15
-  {
     if (pokemonList[i].height >= 15) {
       document.write( "<p>" +
-        pokemonList[i].name + "<br>" +
-          pokemonList[i].height + 
+        pokemonList[i].name + "<br>" + "height:" + " " +
+          pokemonList[i].height + "  " + 
           "-Wow, that's big!" +
           "</p>"
       );
     }
   }
-}
