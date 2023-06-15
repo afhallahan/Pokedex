829c1c63844176f33728a76a171d163f11d9305c
afhallahan <allyson.hallahan@gmail.com>
2023-06-14 22:06:26 -0600

    wrote forEach loop to respond after IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,41 @@
-let pokemonList = [
-  {
-    name: "Kingdra",
-    height: 1.8,
-    types: ["dragon", "water"],
-  },
-  {
-    name: "Wigglytuff",
-    height: 12,
-    types: ["fairy", "normal"],
-  },
-  {
-    name: "Rhydon",
-    height: 120,
-    types: ["rock", "ground"],
-  },
-];
+let pokemonRepository = (function () {
+  //IIFE
+  let pokemonList = [
+    {
+      name: "Kingdra",
+      height: 1.8,
+      types: ["dragon", "water"],
+    },
+    {
+      name: "Wigglytuff",
+      height: 12,
+      types: ["fairy", "normal"],
+    },
+    {
+      name: "Rhydon",
+      height: 120,
+      types: ["rock", "ground"],
+    },
+  ];
 
+  function getAll() {
+    return pokemonList;
+  }
 
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name +  " " + ' is ' + " " + pokemon.height + '<br></br>'); 
-});
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({ name: "Pikachu" });
+console.log(pokemonRepository.getAll()); // [{ name: 'Pikachu' }]
+
+function getAll(pokemon) {
+  document.write (pokemon.name + " " + pokemon.height + "<br></br>");
+}
