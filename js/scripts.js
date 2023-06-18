d85a7abf29e4cbf4afbffc521c0eb6a46de07a87
afhallahan <allyson.hallahan@gmail.com>
2023-06-17 19:36:07 -0600

    remove array

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 let pokemonRepository = (function () {
   //IIFE
   let pokemonList = [
-    {
-      name: "Kingdra",
-      height: 1.8,
-      types: ["dragon", "water"],
-    },
-    {
-      name: "Wigglytuff",
-      height: 12,
-      types: ["fairy", "normal"],
-    },
-    {
-      name: "Rhydon",
-      height: 120,
-      types: ["rock", "ground"],
-    },
   ];
 
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
